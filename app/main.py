a06dd7f4832351f8b835444c63adf1087eaff53f
Амир Абдрафиков <sequze@mail.ru>
2025-07-12 15:17:47 +0300

    add api router

--- app/main.py
+++ app/main.py
@@ -1,3 +1,13 @@
+import uvicorn
 from fastapi import FastAPI
+from api import router as api_router
+from core.config import settings
 
-app = FastAPI
+app = FastAPI()
+app.include_router(
+    api_router,
+    prefix=settings.api_prefix,
+)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
