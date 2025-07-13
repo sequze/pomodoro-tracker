d027c3a688276a510233138a20886f0fbd0cae51
Амир Абдрафиков <sequze@mail.ru>
2025-07-13 21:08:01 +0300

    add lifespan

--- app/main.py
+++ app/main.py
@@ -1,12 +1,21 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 from api import router as api_router
 from core.config import settings
+from core.models.db_helper import db_helper
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(fastapi_app: FastAPI):
+    yield
+    await db_helper.dispose()
+app = FastAPI(
+    lifespan=lifespan,
+)
 app.include_router(
     api_router,
-    prefix=settings.api_prefix,
+    prefix=settings.api_prefix.prefix,
 )
 
 if __name__ == "__main__":
